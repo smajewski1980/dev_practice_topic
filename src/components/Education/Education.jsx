01aaae0ef06f292b4fbd93aec864f906ffaa303f
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-05-09 19:14:36 -0400

    added the cards for the education tab in the browse section

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,5 +1,27 @@
+import styles from "./Education.module.css";
+import CardInfo from "../CardInfo/CardInfo";
+
 const Education = () => {
-  return <div>Education</div>;
+  const cardInfoText =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, unde?";
+  return (
+    <div className={styles.cardWrapper}>
+      <CardInfo
+        title={"Graduation"}
+        icon={{ color: "#80d0c7", number: "80" }}
+        text={cardInfoText}
+        img={"/images/topics/undraw_Graduation_re_gthn.png"}
+        maxWidth={"500px"}
+      />
+      <CardInfo
+        title={"Educator"}
+        icon={{ color: "#80d0c7", number: "75" }}
+        text={cardInfoText}
+        img={"/images/topics/undraw_Educator_re_ju47.png"}
+        maxWidth={"500px"}
+      />
+    </div>
+  );
 };
 
 export default Education;
