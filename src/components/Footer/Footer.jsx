9600dc301ab8164ccdf0ee27c6bed25d6c0596c5
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-05-14 19:44:27 -0400

    added the text to the footer columns

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,9 +7,28 @@ const Footer = () => {
         src='/images/logo.png'
         alt=''
       />
-      <div className={styles.footerCol2}>column 2</div>
-      <div className={styles.footerCol3}>column 3</div>
-      <div className={styles.footerCol4}>column 4</div>
+      <div className={styles.footerCol2}>
+        <h4>Resources</h4>
+        <ul>
+          <li>Home</li>
+          <li>How it wirks</li>
+          <li>FAQs</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.footerCol3}>
+        <h4>Information</h4>
+        <p>305-240-9671</p>
+        <p>info@company.com</p>
+      </div>
+      <div className={styles.footerCol4}>
+        <p>this space will be a dropdown</p>
+        <div>
+          <p>Copyright&copy;2048 Topic Listing Center.</p>
+          <p>All rights reserved.</p>
+        </div>
+        <p>Design: TemplateMo</p>
+      </div>
     </footer>
   );
 };
