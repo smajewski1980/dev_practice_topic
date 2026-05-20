dd2f0a964307d9123cea2dfefdc024cb78f124c2
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-05-20 13:10:12 -0400

    styled the footer and added the lang select popover

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,10 @@ const Footer = () => {
         src='/images/logo.png'
         alt=''
       />
+
       <div className={styles.footerCol2}>
         <h4>Resources</h4>
+
         <ul>
           <li>Home</li>
           <li>How it wirks</li>
@@ -16,17 +18,36 @@ const Footer = () => {
           <li>Contact</li>
         </ul>
       </div>
+
       <div className={styles.footerCol3}>
         <h4>Information</h4>
+
         <p>305-240-9671</p>
         <p>info@company.com</p>
       </div>
+
       <div className={styles.footerCol4}>
-        <p>this space will be a dropdown</p>
+        <button popoverTarget='lang-sel-pop'>
+          English <span>&#9662;</span>
+        </button>
+
+        <div
+          popover={"auto"}
+          id='lang-sel-pop'
+          className={styles.langSelPop}
+        >
+          Another Language <br />
+          Some Other Language
+          <br />
+          Yet Another One
+          <br />
+        </div>
+
         <div>
-          <p>Copyright&copy;2048 Topic Listing Center.</p>
+          <p>Copyright &copy; 2048 Topic Listing Center.</p>
           <p>All rights reserved.</p>
         </div>
+
         <p>Design: TemplateMo</p>
       </div>
     </footer>
